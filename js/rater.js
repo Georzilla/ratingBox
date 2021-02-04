49f41ceba7b2d013140a7e244ae8d3557f7025c4
Georzilla <jfrrj19@gmail.com>
2021-02-04 13:03:13 -0500

    function resetRating that takes default rating and highligt it with highlightRate function

--- js/rater.js
+++ js/rater.js
@@ -1,3 +1,17 @@
 export function Rater(ratingElement) {
     const stars = ratingElement.querySelectorAll('.star');
+
+    const resetRating = ev => {
+        const currentRating = ratingElement.getAttribute('data-rating');
+        highlightRate(currentRating);
+    };
+
+    const highlightRate = (rating) => {
+        stars.forEach(star => {
+            star.style.color = 
+            rating >= star.getAttribute('data-value') ? 'goldenrod' : 'lightslategrey';
+        });
+    }
+
+    resetRating();
 }
