4608c4b94a521821a2650c85dd1a7144d30318d8
Georzilla <jfrrj19@gmail.com>
2021-02-04 14:13:35 -0500

    added function that hover and highlight each star and changes his color styles

--- js/rater.js
+++ js/rater.js
@@ -1,6 +1,11 @@
 export function Rater(ratingElement) {
     const stars = ratingElement.querySelectorAll('.star');
 
+    const ratingHover = ev => {
+        const currentHover = ev.currentTarget.getAttribute('data-value');
+        highlightRate(currentHover);
+    };
+
     const resetRating = ev => {
         const currentRating = ratingElement.getAttribute('data-rating');
         highlightRate(currentRating);
@@ -14,4 +19,11 @@ export function Rater(ratingElement) {
     }
 
     resetRating();
+
+    stars.forEach(star => {
+        star.addEventListener('mouseover', ratingHover);
+    });
+
+    ratingElement.addEventListener('mouseout', resetRating);
+    
 }
